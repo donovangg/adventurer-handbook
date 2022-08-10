6a9b93c6306d9329c22068cd92f5c95185bcab8c
Donovan <donovanjuniogomez@gmail.com>
2022-08-10 01:46:11 -0700

    add spacing to Farmable box

--- pages/components/CharacterImage.js
+++ pages/components/CharacterImage.js
@@ -4,7 +4,7 @@ import { Flex, Spacer, Box, Text, Image } from "@chakra-ui/react";
 export default function CharacterImage({ name, icon }) {
   return (
     <Flex>
-      <Image borderRadius="full" width="64px" src={icon} />
+      <Image margin="1" borderRadius="full" width="64px" src={icon} />
     </Flex>
   );
 }
--- pages/components/Main.js
+++ pages/components/Main.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Flex, Spacer, Box, Text, Image } from "@chakra-ui/react";
+import {
+  Flex,
+  Spacer,
+  Box,
+  Text,
+  Image,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
 import { format, compareAsc } from "date-fns";
 import CharacterImage from "./CharacterImage";
 import Sidebar from "./Sidebar";
@@ -22,79 +30,84 @@ export default function Main({ characters, weapons }) {
         backgroundColor="gray.50"
         paddingTop="10"
       >
-        <Box
-          w="600px"
-          maxH="300px"
-          boxShadow="2xl"
-          rounded="md"
-          backgroundColor="white"
-        >
-          <Text fontSize="3xl">Hi Adventurer</Text>
-        </Box>
-        <Box
-          w="600px"
-          maxH="300px"
-          boxShadow="2xl"
-          rounded="md"
-          backgroundColor="white"
-        >
-          <Text fontSize="3xl">Events Box</Text>
-        </Box>
-        <Box
-          w="400px"
-          maxH="300px"
-          boxShadow="2xl"
-          rounded="md"
-          backgroundColor="gray.400"
-        >
-          <h2>Farmable today</h2>
-          <Flex flexWrap="wrap">
-            {characters.map((character) => (
-              <div key={character.id}>
-                {/* map over array that includes todays date */}
-                {character.farmable_days.includes(today) ? (
-                  <Flex flexDir="column">
-                    {/* <p>{character.name}</p>
+        <Grid templateColumns="repeat(2, 1fr)" gap="10">
+          <Box
+            w="600px"
+            maxH="300px"
+            boxShadow="2xl"
+            rounded="md"
+            backgroundColor="white"
+          >
+            <Text fontSize="3xl">Hi Adventurer</Text>
+          </Box>
+          <Box
+            w="600px"
+            maxH="300px"
+            boxShadow="2xl"
+            rounded="md"
+            backgroundColor="white"
+          >
+            <Text fontSize="3xl">Events Box</Text>
+          </Box>
+        </Grid>
+        <Grid templateColumns="repeat(2, 1fr)" gap="10">
+          <Box
+            w="500px"
+            maxH="300px"
+            boxShadow="2xl"
+            rounded="md"
+            backgroundColor="gray.400"
+            padding="2"
+          >
+            <h2>Farmable today</h2>
+            <Flex flexWrap="wrap">
+              {characters.map((character) => (
+                <div key={character.id}>
+                  {/* map over array that includes todays date */}
+                  {character.farmable_days.includes(today) ? (
+                    <Flex flexDir="column">
+                      {/* <p>{character.name}</p>
                     <Image
                       borderRadius="full"
                       width="64px"
                       src={character.icon}
                     /> */}
-                    <CharacterImage
-                      icon={character.icon}
-                      name={character.name}
-                    />
-                  </Flex>
-                ) : (
-                  ""
-                )}
-              </div>
-            ))}
-          </Flex>
-          <Flex flexWrap="wrap">
-            {weapons.map((weapon) => (
-              <div key={weapon.id}>
-                {/* map over array that includes todays date */}
-                {weapon.farmable_days.includes(today) ? (
-                  <Flex flexDir="column">
-                    <CharacterImage icon={weapon.image} name={weapon.name} />
-                  </Flex>
-                ) : (
-                  ""
-                )}
-              </div>
-            ))}
-          </Flex>
-        </Box>
-        <Box
-          w="400px"
-          maxH="300px"
-          boxShadow="2xl"
-          rounded="md"
-          backgroundColor="white"
-        >
-          <Text fontSize="3xl">Farmable Weapons</Text>
-        </Box>
+                      <CharacterImage
+                        icon={character.icon}
+                        name={character.name}
+                      />
+                    </Flex>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              ))}
+            </Flex>
+            <Flex flexWrap="wrap">
+              {weapons.map((weapon) => (
+                <div key={weapon.id}>
+                  {/* map over array that includes todays date */}
+                  {weapon.farmable_days.includes(today) ? (
+                    <Flex flexDir="column">
+                      <CharacterImage icon={weapon.image} name={weapon.name} />
+                    </Flex>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              ))}
+            </Flex>
+          </Box>
+          <Box
+            w="400px"
+            maxH="300px"
+            boxShadow="2xl"
+            rounded="md"
+            backgroundColor="white"
+          >
+            <Text fontSize="3xl">Farmable Weapons</Text>
+          </Box>
+        </Grid>
       </Flex>
     </Flex>
   );
