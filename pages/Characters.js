716d28f93c008288a13598faf96f7f871f3cf1d1
Donovan Gomez <donovanjuniogomez@gmail.com>
2022-08-22 00:36:41 -0700

    add characters from supabase

--- pages/Characters.js
+++ pages/Characters.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Layout from "../components/Layout";
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Image } from "@chakra-ui/react";
+import { supabase } from "../utils/supabase";
+import CharacterImage from "../components/CharacterImage";
 
-export default function Characters() {
+export default function Characters({characters}) {
   return (
     <Layout>
-      <Flex flex="1" backgroundColor="gray.50">
+      <Flex flexDir="column" flex="1" backgroundColor="gray.50">
         <Text color="black">charactere page</Text>
+        <Flex flexWrap="wrap">
+          {characters.map((character) => (
+            <CharacterImage icon={character.icon} />
+          ))}
+        </Flex>
       </Flex>
     </Layout>
   );
 }
+
+export const getStaticProps = async () => {
+  const { data: characters } = await supabase.from("characters").select("*");
+
+  return {
+    props: {
+      characters,
+    },
+  };
+};
