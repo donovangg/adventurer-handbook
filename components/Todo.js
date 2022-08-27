2f9ad8357b6954441c3a0d1be91c3b9d678430aa
Donovan Gomez <donovanjuniogomez@gmail.com>
2022-08-27 02:46:17 -0700

    add functionality to complete button

--- components/Todo.js
+++ components/Todo.js
@@ -3,16 +3,53 @@ import { Box, Flex, Text, Button, IconButton } from "@chakra-ui/react";
 import { FaCheck, FaTrashAlt } from "react-icons/fa";
 
 export default function Todo({ text, completed, todo, todos, setTodos }) {
-
   const deleteHandler = (e) => {
-    setTodos(todos.filter((el) => el.id !== todo.id))
-  }
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
+
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
 
   return (
-    <Flex width="100%" backgroundColor="purple.100" justifyContent="center" alignItems="center" marginTop="3">
-      <Text flex="1" fontSize="lg">{text}</Text>
-      <IconButton variant="outline" colorScheme="purple" icon={<FaCheck />} />
-      <IconButton onClick={deleteHandler} variant="outline" colorScheme="purple" icon={<FaTrashAlt />} />
+    <Flex
+      width="100%"
+      backgroundColor="purple.100"
+      justifyContent="center"
+      alignItems="center"
+      marginTop="3"
+    >
+      {todo.completed ? (
+        <Text flex="1" opacity="0.5" textDecoration="line-through" fontSize="lg">
+          {text}
+        </Text>
+      ) : (
+        <Text flex="1" fontSize="lg">
+          {text}
+        </Text>
+      )}
+      <IconButton
+        onClick={completeHandler}
+        variant="outline"
+        colorScheme="purple"
+        icon={<FaCheck />}
+      />
+      <IconButton
+        onClick={deleteHandler}
+        variant="outline"
+        colorScheme="purple"
+        icon={<FaTrashAlt />}
+      />
     </Flex>
   );
 }
