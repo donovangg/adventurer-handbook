9350eff071b763a281e6723b9baf3ecbde5bb5a2
Donovan Gomez <donovanjuniogomez@gmail.com>
2022-09-21 01:34:52 -0700

    add dynamic routing to character page

--- pages/characters/[id].js
+++ pages/characters/[id].js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Flex, Box, Text, Image } from "@chakra-ui/react";
+import Layout from "../../components/Layout";
 
 export const getStaticPaths = async () => {
   const res = await fetch("https://api.genshin.dev/characters/all");
   const data = await res.json();
 
   const paths = data.map((character) => {
-    return{
-      params: {id: character.name.toLowerCase()}
-    }
-  })
+    return {
+      params: { id: character.name.toLowerCase() },
+    };
+  });
 
   return {
-    paths: paths, 
-    fallback: false
-  }
-
-}
+    paths: paths,
+    fallback: false,
+  };
+};
 
 export const getStaticProps = async (context) => {
   const id = context.params.id;
@@ -25,17 +26,38 @@ export const getStaticProps = async (context) => {
   return {
     props: {
       character: data,
-    }
-  }
-}
+    },
+  };
+};
 
-export default function details({character}) {
+export default function details({ character }) {
   console.log("details page " + character);
   return (
-    <div>
-      <h1>{character.name}</h1>
-      <h2>{character.vision}</h2>
-      <img src={`https://api.genshin.dev/characters/${character.name.toLowerCase()}/gacha-splash`} />
-    </div>
-  )
+    <Layout>
+      <Flex
+        backgroundColor="gray.50"
+        width="100%"
+        color="black"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        as="main"
+      >
+        <Flex>
+          <Flex>
+            <Flex flexDir="column">
+              <Text>{character.name}</Text>
+              <Text>{character.vision}</Text>
+            </Flex>
+            <Image
+              src={`https://api.genshin.dev/characters/${character.name.toLowerCase()}/icon`}
+              alt={character.name}
+              boxSize="150px"
+              objectFit="cover"
+            />
+          </Flex>
+        </Flex>
+      </Flex>
+    </Layout>
+  );
 }
--- pages/characters/index.js
+++ pages/characters/index.js
@@ -2,27 +2,35 @@ import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import { Flex, Text, Image } from "@chakra-ui/react";
 import { supabase } from "../../utils/supabase";
+import Link from "next/link";
 
-export default function Characters() {
-  const [characters, setCharacters] = useState([]);
+export default function Characters({characters}) {
+ console.log(characters)
 
-  const fetchCharacters = async () => {
-    const res = await fetch("https://api.genshin.dev/characters/all");
-    const data = await res.json();
-    setCharacters(data);
-  };
+//   const fetchCharacters = async () => {
+//     const res = await fetch("https://api.genshin.dev/characters/all");
+//     const data = await res.json();
+//     setCharacters(data);
+//   };
 
-  useEffect(() => {
-    fetchCharacters();
-  }, []);
+//   useEffect(() => {
+//     fetchCharacters();
+//   }, []);
 
   return (
     <Layout>
-      <Flex flexDir="column" flex="1" backgroundColor="gray.50">
+      <Flex flexDir="column" flex="1" color="black" backgroundColor="gray.50">
         <Text color="black">charactere page</Text>
         <Flex flexWrap="wrap">
           {characters.map((character) => (
-            <h2>{character.name}</h2>
+            <div>
+            <Link href={`/characters/${character.name.toLowerCase()}`}>
+            <a>
+              <h2>{character.name}</h2>
+              <Image src={character.icon} alt={character.name} />
+              </a>
+            </Link>
+            </div>
           ))}
         </Flex>
       </Flex>
